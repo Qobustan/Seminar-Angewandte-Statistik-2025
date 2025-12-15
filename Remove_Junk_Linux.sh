35f0c63d0210fd8a6b0d990a92ad2d79baa27d16
Qobustan <115397165+Qobustan@users.noreply.github.com>
2025-12-15 11:38:10 +0100

    Update Remove_Junk_Linux.sh

--- Remove_Junk_Linux.sh
+++ Remove_Junk_Linux.sh
@@ -1,3 +1,4 @@
+# REM Die Dateipfade müsste man anpassen, wenn man das braucht
 rm -r -f *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl **.bcf *.blg *.lot *.out *.xml *.acn *.acr *.alg *.glg *.glo *.gls *.gug *.guo *.gus *.ist *.llg *.llo *.lls *.lug *.luo *.lus *.mog *.moo *.mos *.xdy *.mw
 cd _A_frontmatter/01_frontcover
 rm -r -f *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl **.bcf *.blg *.lot *.out *.xml *.acn *.acr *.alg *.glg *.glo *.gls *.gug *.guo *.gus *.ist *.llg *.llo *.lls *.lug *.luo *.lus *.mog *.moo *.mos *.xdy *.mw

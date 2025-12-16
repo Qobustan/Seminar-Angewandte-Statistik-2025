62e7c6d212a36468b25e455e55402c5b53c15042
Qobustan <115397165+Qobustan@users.noreply.github.com>
2025-12-16 18:42:21 +0100

    Datei erweitert
    Update Remove_Junk_Windows.bat

--- Remove_Junk_Windows.bat
+++ Remove_Junk_Windows.bat
@@ -1,4 +1,8 @@
 REM Die Dateipfade müsste man anpassen, wenn man das braucht
-del *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl **.bcf *.blg *.lot *.out *.xml *.acn *.acr *.alg *.glg *.glo *.gls *.gug *.guo *.gus *.ist *.llg *.llo *.lls *.lug *.luo *.lus *.mog *.moo *.mos *.xdy *.mw
+del *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl **.bcf *.blg *.lot *.out *.xml *.acn *.acr *.alg *.glg *.glo *.gls *.gug *.guo *.gus *.ist *.llg *.llo *.lls *.lug *.luo *.lus *.mog *.moo *.mos *.xdy *.mw *.synctex.gz *.fdb_latexmk *.fls *.idx *.ind *.ilg *.glg *.glo *.gls *.dvi *.ps *.pdf
+
+REM Wechsel in das Verzeichnis 'Ausarbeitung'
 chdir Ausarbeitung
-del *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl **.bcf *.blg *.lot *.out *.xml *.acn *.acr *.alg *.glg *.glo *.gls *.gug *.guo *.gus *.ist *.llg *.llo *.lls *.lug *.luo *.lus *.mog *.moo *.mos *.xdy *.mw
+
+REM Löschen der gleichen temporären Dateien im Zielverzeichnis
+del *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl **.bcf *.blg *.lot *.out *.xml *.acn *.acr *.alg *.glg *.glo *.gls *.gug *.guo *.gus *.ist *.llg *.llo *.lls *.lug *.luo *.lus *.mog *.moo *.mos *.xdy *.mw *.synctex.gz *.fdb_latexmk *.fls *.idx *.ind *.ilg *.glg *.glo *.gls *.dvi *.ps *.pdf

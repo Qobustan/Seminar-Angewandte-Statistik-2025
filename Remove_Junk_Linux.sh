662a875a474ba715753fe9431f827daef0606df0
Qobustan <115397165+Qobustan@users.noreply.github.com>
2025-12-16 18:46:48 +0100

    Shell an Batch angeglichen
    Update Remove_Junk_Linux.sh

--- Remove_Junk_Linux.sh
+++ Remove_Junk_Linux.sh
@@ -1,4 +1,22 @@
-# REM Die Dateipfade müsste man anpassen, wenn man das braucht
+#!/bin/bash
+
+# Kontrollvariable für die Ausgabe von Kommentaren
+SHOW_COMMENTS=true
+
+# Die Dateipfade müsste man anpassen, wenn man das braucht
+if [ "$SHOW_COMMENTS" = true ]; then
+    echo "# Die Dateipfade müsste man anpassen, wenn man das braucht"
+fi
 rm -r -f *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl **.bcf *.blg *.lot *.out *.xml *.acn *.acr *.alg *.glg *.glo *.gls *.gug *.guo *.gus *.ist *.llg *.llo *.lls *.lug *.luo *.lus *.mog *.moo *.mos *.xdy *.mw
+
+# Wechsel in das Verzeichnis 'Ausarbeitung'
+if [ "$SHOW_COMMENTS" = true ]; then
+    echo "# Wechsel in das Verzeichnis 'Ausarbeitung'"
+fi
 cd Ausarbeitung
+
+# Löschen der gleichen temporären Dateien im Zielverzeichnis
+if [ "$SHOW_COMMENTS" = true ]; then
+    echo "# Löschen der gleichen temporären Dateien im Zielverzeichnis"
+fi
 rm -r -f *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl **.bcf *.blg *.lot *.out *.xml *.acn *.acr *.alg *.glg *.glo *.gls *.gug *.guo *.gus *.ist *.llg *.llo *.lls *.lug *.luo *.lus *.mog *.moo *.mos *.xdy *.mw

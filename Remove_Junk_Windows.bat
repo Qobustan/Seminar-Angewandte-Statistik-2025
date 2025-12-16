d1c095b2f20cf26e5f250b139df605138aab4029
Qobustan <115397165+Qobustan@users.noreply.github.com>
2025-12-16 18:45:15 +0100

    Optionale Ausgaben eingeführt
    Update Remove_Junk_Windows.bat

--- Remove_Junk_Windows.bat
+++ Remove_Junk_Windows.bat
@@ -1,8 +1,14 @@
+@echo off
+set SHOW_COMMENTS=true
+
 REM Die Dateipfade müsste man anpassen, wenn man das braucht
+if "%SHOW_COMMENTS%"=="true" echo REM Die Dateipfade müsste man anpassen, wenn man das braucht
 del *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl **.bcf *.blg *.lot *.out *.xml *.acn *.acr *.alg *.glg *.glo *.gls *.gug *.guo *.gus *.ist *.llg *.llo *.lls *.lug *.luo *.lus *.mog *.moo *.mos *.xdy *.mw *.synctex.gz *.fdb_latexmk *.fls *.idx *.ind *.ilg *.glg *.glo *.gls *.dvi *.ps *.pdf
 
 REM Wechsel in das Verzeichnis 'Ausarbeitung'
+if "%SHOW_COMMENTS%"=="true" echo REM Wechsel in das Verzeichnis 'Ausarbeitung'
 chdir Ausarbeitung
 
 REM Löschen der gleichen temporären Dateien im Zielverzeichnis
+if "%SHOW_COMMENTS%"=="true" echo REM Löschen der gleichen temporären Dateien im Zielverzeichnis
 del *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl **.bcf *.blg *.lot *.out *.xml *.acn *.acr *.alg *.glg *.glo *.gls *.gug *.guo *.gus *.ist *.llg *.llo *.lls *.lug *.luo *.lus *.mog *.moo *.mos *.xdy *.mw *.synctex.gz *.fdb_latexmk *.fls *.idx *.ind *.ilg *.glg *.glo *.gls *.dvi *.ps *.pdf
